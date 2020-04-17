2bc866de062a885f2fb6130111ab659a1227fc5e
egolive <Daniel@Ehrenhofer.net>
2020-04-17 21:28:13 +0200

    init commit

---
+++ database/factories/ClubFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Club;
+use Faker\Generator as Faker;
+
+$factory->define(Club::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
