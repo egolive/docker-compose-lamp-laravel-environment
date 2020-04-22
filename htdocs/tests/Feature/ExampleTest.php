a31baf682432f8dd45f9ceb2529a3fafac4e9875
egolive <Daniel@Ehrenhofer.net>
2020-04-22 23:54:30 +0200

    unit test fix

--- htdocs/tests/Feature/ExampleTest.php
+++ htdocs/tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/club');
 
         $response->assertStatus(200);
     }
